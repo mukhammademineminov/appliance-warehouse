4398813a3953fa5b8d633505e535957c528eda09
mukhammademin <mukhammademin19@icloud.com>
2025-12-06 14:11:23 +0500

    dal: add repository and factory

--- ApplianceWarehouse.Domain/obj/Debug/net8.0/ApplianceWarehouse.Domain.AssemblyInfo.cs
+++ ApplianceWarehouse.Domain/obj/Debug/net8.0/ApplianceWarehouse.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApplianceWarehouse.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+738c7b51abd840c6c7e03075bb5f48061eac0283")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApplianceWarehouse.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApplianceWarehouse.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
